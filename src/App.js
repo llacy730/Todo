445c10d7e97ab882dffec6774d7e885fb51b7dd6
Lee Lacy <llacy730@gmail.com>
2020-01-19 14:16:45 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from './components/navbar'
+import TodoList from './components/Todolist'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App
